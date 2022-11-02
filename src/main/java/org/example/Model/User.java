c384774afac9040a9480f9da8302cf8fee4f20fd
RobertSlensvik <78215952+RobertSlensvik@users.noreply.github.com>
2022-11-02 15:57:08 +0100

    Adding user and bank balance

--- src/main/java/org/example/Model/User.java
+++ src/main/java/org/example/Model/User.java
@@ -4,6 +4,10 @@ public class User {
     private String name;
     private double bankBalance;
 
+    public User(){
+
+    }
+
     public User(String name, double bankBalance){
         this.name = name;
         this.bankBalance = bankBalance;
@@ -11,5 +15,13 @@ public class User {
     public String getName(){
         return name;
     }
-    
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getBalance(){
+        return bankBalance;
+    }
+    public void setBalance(double bankBalance){
+        this.bankBalance = bankBalance;
+    }
 }
